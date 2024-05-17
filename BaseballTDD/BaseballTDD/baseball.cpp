1a873e9d953fc409c053c10bf8acc0e00060767f
mj <mj@aa.com>
2024-05-17 14:48:18 +0900

    update tset

--- BaseballTDD/BaseballTDD/baseball.cpp
+++ BaseballTDD/BaseballTDD/baseball.cpp
@@ -2,14 +2,39 @@
 #include <string>
 using namespace std;
 
+struct GuessResult{
+	int strike = 0;
+	int ball = 0;
+};
+
 class Baseball {
 public:
-	void guess(const string& str) {
+	void SetQuestion(const string& str) {
 		if (!isValidInput(str)) {
 			throw invalid_argument("Invalid input");
 		}
+		q = str;
+	}
+
+	GuessResult guess(const string& str) {
+		if (!isValidInput(str)) {
+			throw invalid_argument("Invalid input");
+		}
+		// compare q and str
+		GuessResult result;
+		for (int i = 0; i < 3; i++) {
+			if (q[i] == str[i]) {
+				result.strike++;
+			}
+			else if (q.find(str[i]) != string::npos) {
+				result.ball++;
+			}
+		}
+		return result;
+
 	}
 private:
+	string q;
 	bool isValidInput(const string& str) {
 
 		if (str.length() != 3) {
--- BaseballTDD/Sample-Test1/test.cpp
+++ BaseballTDD/Sample-Test1/test.cpp
@@ -3,6 +3,7 @@
 
 class BaseballFixture : public ::testing::Test {
 public:
+
 	Baseball baseball;
 
 	void assertIllegalInput(const string& str) {
@@ -14,9 +15,33 @@ public:
 		}
 		FAIL() << "Expected: Invalid input"; // 예외가 발생하지 않으면 테스트 실패
 	}
+private:
+	string question;
 };
 TEST_F(BaseballFixture, IllegalInput) {
 	assertIllegalInput("1234");
 	assertIllegalInput("12a");
 	assertIllegalInput("abc");
+}
+
+TEST_F(BaseballFixture, NoMatch) {
+	baseball.SetQuestion("123");
+	auto result = baseball.guess("456");
+	ASSERT_EQ(0, result.strike);
+	ASSERT_EQ(0, result.ball);
+}
+
+TEST_F(BaseballFixture, Strike2) {
+	baseball.SetQuestion("123");
+	auto result = baseball.guess("124");
+	ASSERT_EQ(2, result.strike);
+	ASSERT_EQ(0, result.ball);
+}
+
+
+TEST_F(BaseballFixture, Strike1Ball2) {
+	baseball.SetQuestion("123");
+	auto result = baseball.guess("132");
+	ASSERT_EQ(1, result.strike);
+	ASSERT_EQ(2, result.ball);
 }
